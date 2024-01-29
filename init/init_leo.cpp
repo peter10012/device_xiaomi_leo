87f03c247653fe12d049c99ddf834387a38cf670
dkmos2016 <dkmos2016@126.com>
2024-01-29 13:46:00 +0800

    leo: update init

--- init/init_leo.cpp
+++ init/init_leo.cpp
@@ -28,6 +28,11 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <sys/sysinfo.h>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <android-base/properties.h>
+#include <sys/_system_properties.h>
+#include <stdio.h>
+#include <sys/system_properties.h>
 
 #include "vendor_init.h"
 #include "property_service.h"
@@ -36,33 +41,40 @@
 char const *heapgrowthlimit;
 char const *heapminfree;
 
-using android::init::property_set;
+using android::base::GetProperty;
+int property_set(const char *key, const char *value)
+{
+  return __system_property_set(key, value);
+}
 
 void check_device()
 {
-    struct sysinfo sys;
+  struct sysinfo sys;
 
-    sysinfo(&sys);
+  sysinfo(&sys);
 
-    if (sys.totalram > 3072ull * 1024 * 1024) {
-        // from - Stock rom
-        heapgrowthlimit = "256m";
-        heapminfree = "4m";
-    } else {
-        // from - phone-xxhdpi-2048-dalvik-heap.mk
-        heapgrowthlimit = "192m";
-        heapminfree = "2m";
-   }
+  if (sys.totalram > 3072ull * 1024 * 1024)
+  {
+    // from - Stock rom
+    heapgrowthlimit = "256m";
+    heapminfree = "4m";
+  }
+  else
+  {
+    // from - phone-xxhdpi-2048-dalvik-heap.mk
+    heapgrowthlimit = "192m";
+    heapminfree = "2m";
+  }
 }
 
 void vendor_load_properties()
 {
-    check_device();
+  check_device();
 
-    property_set("dalvik.vm.heapstartsize", "16m");
-    property_set("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
-    property_set("dalvik.vm.heapsize", "512m");
-    property_set("dalvik.vm.heaptargetutilization", "0.75");
-    property_set("dalvik.vm.heapminfree", heapminfree);
-    property_set("dalvik.vm.heapmaxfree", "8m");
+  property_set("dalvik.vm.heapstartsize", "16m");
+  property_set("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
+  property_set("dalvik.vm.heapsize", "512m");
+  property_set("dalvik.vm.heaptargetutilization", "0.75");
+  property_set("dalvik.vm.heapminfree", heapminfree);
+  property_set("dalvik.vm.heapmaxfree", "8m");
 }

ead4eb3492d2adf46bad077cc9260f90731e770b
dkmos2016 <dkmos2016@126.com>
2024-01-29 16:03:05 +0800

    leo: Redfin March

--- revengeos_leo.mk
+++ revengeos_leo.mk
@@ -45,4 +45,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="leo-user 11.0 NRD90M V9.6.1.0.NCFMIFD release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ1A.210205.004/7038034:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210305.006/7119741:user/release-keys"

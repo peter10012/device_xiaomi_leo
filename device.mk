c2e5be5aa632361e9b0053dfdaf6ec22b388bdbd
dkmos2016 <dkmos2016@126.com>
2024-01-29 16:02:24 +0800

    leo: Import AIDL PowerHAL

--- device.mk
+++ device.mk
@@ -326,7 +326,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service
+    android.hardware.power-service-leo
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \

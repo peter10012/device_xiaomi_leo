56f93d3f83769793564fc2f51fbde11ce845bb20
dkmos2016 <dkmos2016@126.com>
2024-01-29 14:04:07 +0800

    leo: prop: revert dalvik config changes

--- prop.mk
+++ prop.mk
@@ -12,7 +12,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 dalvik.vm.heapstartsize=8m \
 dalvik.vm.heapgrowthlimit=256m \
 dalvik.vm.heapsize=512m \
-dalvik.vm.heaptargetutilization=0.75 \
+dalvik.vm.heaptargetutilization=0.9 \
 dalvik.vm.heapminfree=8m \
 dalvik.vm.heapmaxfree=16m
 
